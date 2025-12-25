13cce69bd5539a3d5c6ebbddbc649b265cf30da9
Manasi <Manasiavachat14@gmail.com>
2025-12-25 12:45:28 +0530

    Create Config Manager Class for managing multiple environments(dev,qa,uat) for the project
    updated pom.xml updated maven compiler source and target to 14.

--- src/test/java/com/api/test/LoginAPITest.java
+++ src/test/java/com/api/test/LoginAPITest.java
@@ -1,22 +1,27 @@
 package com.api.test;
 
-import static io.restassured.RestAssured.*;
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.lessThan;
 
-import static org.hamcrest.Matchers.*;
+import java.io.IOException;
 
 import org.testng.annotations.Test;
 
 import com.api.pojo.UserCredentials;
+import com.api.utils.ConfigManager;
 
 import io.restassured.http.ContentType;
 import io.restassured.module.jsv.JsonSchemaValidator;
 
 public class LoginAPITest {
 	@Test
-	public void loginTest() {
+	public void loginTest() throws IOException {
+		
+		
 		UserCredentials userCredentials = new UserCredentials("iamfd", "password") ;
 		given()
-			.baseUri("http://64.227.160.186:9000/v1")
+			.baseUri(ConfigManager.getProperty("BASE_URI"))
 			.and()
 			.contentType(ContentType.JSON)
 			.and()
---
+++ src/test/java/com/api/utils/ConfigManagerOld.java
@@ -0,0 +1,34 @@
+package com.api.utils;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Properties;
+
+public class ConfigManagerOld {
+
+	private static Properties prop = new Properties();
+
+	static {
+
+		File configFile = new File(
+				System.getProperty("user.dir") + "\\src\\test\\resources\\config\\config.properties");
+		FileReader fileReader = null;
+		try {
+			fileReader = new FileReader(configFile);
+			prop.load(fileReader);
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+	}
+
+	public static String getProperty(String key) {
+
+		return prop.getProperty(key);
+
+	}
+}
