41682c94f73b65e74824c05914ed37b076e87ed9
Manasi <Manasiavachat14@gmail.com>
2026-01-20 17:46:02 +0530

    Refactored  the package com.api.pojo to com.api.request.model

--- src/test/java/com/api/test/SpecUtil.java
+++ src/test/java/com/api/test/SpecUtil.java
@@ -5,7 +5,7 @@ import static com.api.utils.ConfigManager.*;
 import org.hamcrest.Matchers;
 
 import com.api.constant.Role;
-import com.api.pojo.UserCredentials;
+import com.api.request.model.UserCredentials;
 import com.api.utils.AuthTokenProvider;
 
 import io.restassured.builder.RequestSpecBuilder;
