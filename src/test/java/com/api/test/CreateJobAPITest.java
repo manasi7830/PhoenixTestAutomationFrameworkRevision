36b69efab40e798a4aa797dce5a3185bb93bb29c
Manasi <Manasiavachat14@gmail.com>
2026-01-24 19:51:39 +0530

    Introduced Enums for Handling the codes for the CreateJobAPI test reduce the magic number issue in the code

--- src/test/java/com/api/test/CreateJobAPITest.java
+++ src/test/java/com/api/test/CreateJobAPITest.java
@@ -9,7 +9,14 @@ import java.util.List;
 
 import org.testng.annotations.Test;
 
+import com.api.constant.Model;
+import com.api.constant.OEM;
+import com.api.constant.Platform;
+import com.api.constant.Problem;
+import com.api.constant.Product;
 import com.api.constant.Role;
+import com.api.constant.ServiceLocation;
+import com.api.constant.Warrenty_Status;
 import com.api.request.model.CreateJobPayload;
 import com.api.request.model.Customer;
 import com.api.request.model.CustomerAddress;
@@ -27,11 +34,11 @@ public class CreateJobAPITest {
 	
 		Customer customer=new Customer("Manasi","Avachat","9767145100"," ","manasiavachat14@gmail.com","");
 		CustomerAddress customerAddress=new CustomerAddress("20B","Atria","HMT","Jalahalli","Bangalore","560013","India","Karnataka");
-		CustomerProduct customerProduct=new CustomerProduct(getTimeWithDayAgo(10),"58118034553133","58118034553133","58118034553133",getTimeWithDayAgo(10),1,1);
-		Problems problems=new Problems(1,"Battery Issue");
+		CustomerProduct customerProduct=new CustomerProduct(getTimeWithDayAgo(10),"55118034553133","55118034553133","55118034553133",getTimeWithDayAgo(10),Product.NEXUS_2.getCode(),Model.NEXUS_2_BLUE.getCode());
+		Problems problems=new Problems(Problem.POOR_BATTERY_LIFE.getCode(),"Battery Issue");
 		List<Problems> problemList=new ArrayList<Problems>();
 		problemList.add(problems);
-		CreateJobPayload createJobPayload=new CreateJobPayload(0,2 ,1 ,1 , customer, customerAddress, customerProduct, problemList);
+		CreateJobPayload createJobPayload=new CreateJobPayload(ServiceLocation.SERVICE_LOCATION_A.getCode(),Platform.FRONT_DESK.getCode() ,Warrenty_Status.IN_WARRENTY.getCode() ,OEM.GOOGLE.getCode(), customer, customerAddress, customerProduct, problemList);
 		
 		given()
 		.spec(SpecUtil.requestSpecWithAuth(Role.FD, createJobPayload))
